039c5847db793de3a749cdfd3ba873ed10a0dc40
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-12-05 11:24:15 +0800

    add authenticate_user to admin::posts controller to fix admin check method

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,7 +1,9 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :authenticate_admin
 
   def index
     @posts = Post.order(created_at: :desc).page(params[:page]).per(50)
   end
+
 end
