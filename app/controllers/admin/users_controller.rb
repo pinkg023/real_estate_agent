7cdf6ee69978a08e4ac563b187f30038058aff17
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-12-10 21:01:09 +0800

    add control tag in control panel

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,36 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
+  def show
+    set_user
+  end
+
+  def edit
+    set_user
+  end
+
+  def index
+    @users = User.order(created_at: :desc).page(params[:page]).per(20)
+  end
+
+  def update
+    set_user
+      if @user.update(user_params)
+        flash[:notice] = "會員資料已更新"    
+      else
+        render edit_user_path(@user)
+        flash[:alert] = @user.errors.full_messages.to_sentence
+      end
+      redirect_to admin_users_path
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :role, :name, :tel, :cel)
+    end
+end
