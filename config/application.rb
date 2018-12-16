45e9ee635177b73b1018e29c9cd520597f6b030f
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-12-16 21:39:12 +0800

    add config.i18n.fallbacks = [I18n.default_locale]

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module RealEstateAgent
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
+    config.i18n.fallbacks = [I18n.default_locale]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
