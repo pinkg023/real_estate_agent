6f232d6cd3f74d8287e8159b8d171edba7ebfc92
pinkg023 <pinkg023@g.ncu.edu.tw>
2019-01-19 15:18:19 +0800

    edit sitemap.rb

--- sitemap.rb
+++ sitemap.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'https://https://luckyagent.herokuapp.com'
+SitemapGenerator::Sitemap.default_host = 'https://luckyagent.herokuapp.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
   add '/contact_us', :changefreq => 'weekly'
